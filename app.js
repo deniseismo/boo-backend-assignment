e9ac025222d09b7f8cb97f18d8c5aba7db1a629b
deniseismo <denisseismo@gmail.com>
2024-03-06 22:25:08 +0700

    register profile routes in the app

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const profileRoutes = require("./routes/profileRoutes");
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-// routes
-app.use('/', require('./routes/profile')());
+// profile routes
+app.use("/profile", profileRoutes);
 
 // start server
 const server = app.listen(port);
-console.log('Express started. Listening on %s', port);
+console.log("Express started. Listening on %s", port);
