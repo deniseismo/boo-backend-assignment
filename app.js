473c1e1bf37c9af8662cafc4014a66ca0b5ca52a
deniseismo <denisseismo@gmail.com>
2024-03-07 23:47:34 +0700

    add environmental variables, add comments, refactor

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 "use strict";
 
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import profileRoutes from "./routes/profileRoutes.js";
+
 const app = express();
+
+// prerequisites: environmental variable for setting up a mongodb server
 const port = process.env.PORT || 3000;
-const profileRoutes = require("./routes/profileRoutes");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbAdress = process.env.DB_ADDRESS;
+const dbName = process.env.DB_NAME;
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-// profile routes
+// connect to mongoDB
+const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbAdress}`;
+
+// make sure it's not connected to a real db while testing
+if (process.env.NODE_ENV !== "test") {
+  mongoose
+    .connect(dbURI, {
+      dbName: dbName,
+    })
+    .then((result) => {
+      app.listen(3000);
+      console.log("Express started. Listening on %s", port);
+    })
+    .catch((err) => console.log(err));
+}
+
+// register profile routes
 app.use("/profile", profileRoutes);
 
-// start server
-const server = app.listen(port);
-console.log("Express started. Listening on %s", port);
+export default app;
